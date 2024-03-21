15968e15ff90a3a8801b0bf9a73da42fd9a29daa
黃彥博 <zark.huang@everfortuneai.com.tw>
2024-03-21 15:09:56 +0800

    add vercel.json

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,7 +1,31 @@
+// module.exports = [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [
+        "https://socialmedial-officail-bu7sxyc9y-zarks-projects-08240bb7.vercel.app",
+      ],
+      headers: "*",
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      credentials: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
@@ -10,27 +34,3 @@ module.exports = [
   "strapi::favicon",
   "strapi::public",
 ];
-
-// module.exports = [
-//   "strapi::errors",
-//   "strapi::security",
-//   {
-//     name: "strapi::cors",
-//     config: {
-//       enabled: true,
-//       origin: [
-//         "https://socialmedial-officail-bu7sxyc9y-zarks-projects-08240bb7.vercel.app",
-//       ],
-//       headers: "*",
-//       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//       credentials: true,
-//     },
-//   },
-//   "strapi::poweredBy",
-//   "strapi::logger",
-//   "strapi::query",
-//   "strapi::body",
-//   "strapi::session",
-//   "strapi::favicon",
-//   "strapi::public",
-// ];
